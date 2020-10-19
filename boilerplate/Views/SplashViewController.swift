36f3e722f118a78186e20731c3bc05e7d863b216
PedroGaN <pgarcianavalon@gmail.com>
2020-10-19 20:23:34 +0100

    Added TabBarController "HomeViewController"

--- boilerplate/Views/SplashViewController.swift
+++ boilerplate/Views/SplashViewController.swift
@@ -18,7 +18,8 @@ class SplashViewController: UIViewController{
     override func viewDidAppear(_ animated: Bool) {
         
         //if we don't have valid session
-        self.performSegue(withIdentifier: "goToLogin", sender: nil)
+        //self.performSegue(withIdentifier: "goToLogin", sender: nil)
+        self.performSegue(withIdentifier: "goToHome", sender: nil)
         
     }
 }
